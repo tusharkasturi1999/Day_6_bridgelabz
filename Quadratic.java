31ac53748ef50569ab4e284a1e1b90fe2d5fed71
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-19 23:21:39 +0530

    Added Quadratic

---
+++ Quadratic.java
@@ -0,0 +1,22 @@
+package com.day8PracticeProblems;
+
+import java.util.Scanner;
+
+public class Quadratic {
+	public static void quadratic(double a, double b, double c) {
+		double delta = (b*b)-(4*a*c);
+		double x1 = (-b+Math.sqrt(delta))/(2*a);
+		double x2 = (-b-Math.sqrt(delta))/(2*a);
+		System.out.println("Root1 of x is : "+x1);
+		System.out.println("Root2 of x is : "+x2);
+	}
+	public static void main( String[] args ) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter values for a, b & c respectively :");
+		double a=sc.nextDouble();
+		double b=sc.nextDouble();
+		double c=sc.nextDouble();
+		quadratic(a,b,c);
+	}
+
+}

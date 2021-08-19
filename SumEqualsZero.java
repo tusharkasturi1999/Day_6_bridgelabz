ef6257253b5cb7f5fa02140f996c406f2c79d837
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-19 23:52:13 +0530

    Added SumEqualsZero

---
+++ SumEqualsZero.java
@@ -0,0 +1,40 @@
+package com.day8PracticeProblems;
+
+import java.util.Scanner;
+
+public class SumEqualsZero {
+	public static void sumequalszero(int[] arr, int n) {
+		boolean found = false;
+		int count = 0;
+		for(int i=0; i<n-2;i++) {
+			for(int j=i+1;j<n-1;j++) {
+				for(int k=j+1;j<n;j++) {
+					if(arr[i]+arr[j]+arr[k]==0) {
+						count++;
+						System.out.println("Triplets are "+arr[i]+","+arr[j]+","+arr[k]);
+						found = true;
+					}
+					
+					
+				}
+			}
+		}
+		if(found==false) {
+			System.out.println("No triplets found");
+		}
+		System.out.println("Total triplet pairs : "+count);
+	}
+	public static void main( String[] args ) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of elements in the array : ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter the elements of the array :");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		sumequalszero(arr,n);
+		sc.close();
+	}
+
+}

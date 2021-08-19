6eb59ace70286cb24823eced17927ee8ac439815
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-19 20:24:05 +0530

    Added EvenOdd

---
+++ EvenOdd.java
@@ -0,0 +1,21 @@
+package com.day8PracticeProblems;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+	public static void main( String[] args ) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to check : ");
+		int number = sc.nextInt();
+		if(number%2==0) {
+			System.out.println("The number "+number+" is Even");
+		}
+		else {
+			System.out.println("The number "+number+" is ODD");
+		}
+		
+		
+	}
+
+}

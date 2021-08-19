7a657d58dd3a2f333c0b431cfc38ebd49635aa67
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-19 20:03:16 +0530

    Added QuotientRemainder

---
+++ QuotientRemainder.java
@@ -0,0 +1,22 @@
+package com.day8PracticeProblems;
+
+import java.util.Scanner;
+
+public class QuotientRemainder {
+	public static void main( String[] args ) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number to be Divided ");
+		int Number = sc.nextInt();
+		System.out.println("Enter the Divisor ");
+		int Divisor = sc.nextInt();
+		
+		double Quotient = Number/Divisor;
+		double Remainder = (Number)%Divisor;
+		
+		System.out.println("Quotient : "+Quotient);
+		System.out.println("Remainder : "+Remainder);
+	}
+
+}

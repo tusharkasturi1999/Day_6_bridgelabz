23417445bd274cdcc5f1baaab1cfaf1e3d232bdd
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-19 18:54:13 +0530

    Added PowerOfTwo

---
+++ PowerOfTwo.java
@@ -0,0 +1,19 @@
+package com.day8PracticeProblems;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+	public static void main( String[] args ) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter value ofthe power : ");
+		int N =Math.abs(sc.nextInt());
+		
+		for (int i=0; i<= N; i++)
+		{
+			System.out.println((int)Math.pow(2, i));
+		}
+		
+	}
+
+}
